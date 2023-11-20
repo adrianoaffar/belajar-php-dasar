92d4a73bc6467373cec35b7e3a64896c91b62529
unknown <adriano.affar28@gmail.com>
2023-11-20 19:51:56 +0900

    php

---
+++ Pertemuan3/latihan2.php
@@ -0,0 +1,18 @@
+<?php 
+// pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// swtich
+
+$x = 10;
+if( $x < 4 ){
+    echo"benar";
+} else if ( $x == 20 ) {
+    echo"bingo";
+}
+else{
+    echo "salah";
+}
+
+?>
---
+++ Pertemuan4/date.php
@@ -0,0 +1,21 @@
+<?php 
+// Date
+// untuk menampilkan tanggal dengan format tertentu
+    echo date("l, d-M-Y");
+
+// Time
+// UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 januari 1970
+echo time();
+echo date("l",time()-60*60*24*100);
+
+// mk time
+// membuat sendiri detik
+// mktime (0,0,0,0,0,0)
+// jam, menit, detik, bulan tanggal tahun
+echo date("l", mktime(0,0,0,6,28,2004));
+
+// stortime
+echo date("l", strtotime("28 jun 2004"));
+
+?>
